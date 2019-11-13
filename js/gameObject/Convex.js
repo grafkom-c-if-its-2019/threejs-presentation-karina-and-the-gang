4e212e42dab0b0d131f79dc68c9c347313bc5f0d
atikakiki <atikarizki41@gmail.com>
2019-11-13 23:19:46 +0700

    tambahan

--- js/gameObject/Convex.js
+++ js/gameObject/Convex.js
@@ -8,33 +8,26 @@ class Convex extends GameObject{
 
     CreateMesh(){
         var _data = this.data;
-        this.InitGeometry();
-        this.InitMaterial();
-        // this.mesh = new THREE.SceneUtils.createMultiMaterialObject(this.geometry, [this.meshMaterial, this.wireFrameMat]);
-        this.mesh = new THREE.Mesh(this.geometry, this.material);
-        this.mesh.position.copy(_data.position);
-    }
 
-    InitGeometry(){        
-        this.points.push(new THREE.Vector3(0, 1, 0));
-        this.points.push(new THREE.Vector3(0, -2, 0));
-        this.points.push(new THREE.Vector3(1, -1, 0));
-        this.points.push(new THREE.Vector3(-1, -1, 0));
-        this.points.push(new THREE.Vector3(0, 0, 1));
+        //points
+        this.points.push(new THREE.Vector3(0, 2, 0));
+        this.points.push(new THREE.Vector3(0, -3, 0));
+        this.points.push(new THREE.Vector3(2, -2, 0));
+        // this.points.push(new THREE.Vector3(-2, -2, 0));
+        // this.points.push(new THREE.Vector3(0, 0, 2));
         this.points.push(new THREE.Vector3(0, 0, -1));
-        this.points.push(new THREE.Vector3(0, 0, 0));
+        // this.points.push(new THREE.Vector3(0, 0,1));
+        // this.points.push(new THREE.Vector3(2, 0, 2));
 
         this.geometry = new THREE.ConvexGeometry(this.points);
-    }
-
-    InitMaterial(){
-        this.meshMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00, transparent: true, opacity: 0.2});
-        this.meshMaterial.side = THREE.DoubleSide;
-
-        this.wireFrameMat = new THREE.MeshBasicMaterial();
-        this.wireFrameMat.wireframe = true;
-
+        
+        //create mesh
+        // this.meshMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00, transparent: true, opacity: 0.2});
+        // this.meshMaterial.side = THREE.DoubleSide;
         this.material = new THREE.MeshNormalMaterial();
+        this.material.wireframe = true;
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+        this.mesh.position.copy(_data.position);
     }
 
     Update(){
--- js/gameObject/Lathe.js
+++ js/gameObject/Lathe.js
@@ -8,33 +8,22 @@ class Lathe extends GameObject{
 
     CreateMesh(){
         var _data = this.data;
-        this.InitGeometry();
-        this.InitMaterial();
+        
+        //geometry points
+        // this.points.push(new THREE.Vector3(2, 0, 0));
+        this.points.push(new THREE.Vector3(1, 1, 0));
+        this.points.push(new THREE.Vector3(1, -1, 0));
+        // this.points.push(new THREE.Vector3(2, -1, 0));
+
+        this.geometry = new THREE.LatheGeometry(this.points, _data.segments, _data.phiStart, _data.phiLen);
+
+        //create mesh
+        this.material = new THREE.MeshNormalMaterial();
+        this.material.wireframe = true;
         // this.mesh = new THREE.SceneUtils.createMultiMaterialObject(this.geometry, [this.meshMaterial, this.wireFrameMat]);
         this.mesh = new THREE.Mesh(this.geometry, this.material);
         this.mesh.position.copy(_data.position);
     }
-
-    InitGeometry(){
-        var _data = this.data;       
-        this.points.push(new THREE.Vector3(2, 0, 0));
-        this.points.push(new THREE.Vector3(1, 1, 0));
-        this.points.push(new THREE.Vector3(1, -1, 0));
-        this.points.push(new THREE.Vector3(2, 0, 0));
-
-        this.geometry = new THREE.LatheGeometry(this.points, _data.segments, _data.phiStart, _data.phiLen);
-    }
-
-    InitMaterial(){
-        this.meshMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00, transparent: true, opacity: 0.2});
-        this.meshMaterial.side = THREE.DoubleSide;
-
-        this.wireFrameMat = new THREE.MeshBasicMaterial();
-        this.wireFrameMat.wireframe = true;
-
-        this.material = new THREE.MeshNormalMaterial();
-    }
-
     Update(){
         
     }
